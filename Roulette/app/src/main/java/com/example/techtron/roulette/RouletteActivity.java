4fc5ed32e93fa7d7a6c62a0d8cec936201f8a416
Phillip <phillipables1@gmail.com>
2018-02-14 21:42:47 -0500

    instatiat int for r number and a placeholder for the old int which equals degree int modulous 360

--- Roulette/app/src/main/java/com/example/techtron/roulette/RouletteActivity.java
+++ Roulette/app/src/main/java/com/example/techtron/roulette/RouletteActivity.java
@@ -1,16 +1,21 @@
 package com.example.techtron.roulette;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import java.util.Random;
 
 public class RouletteActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     ImageView wheelRoul;
+
+    Random r;
+    int degree = 0, degree_old = 0;
 
 
     @Override
@@ -21,10 +26,12 @@ public class RouletteActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         wheelRoul = (ImageView) findViewById(R.id.imRoulette);
 
+        r = new Random();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                degree_old = degree % 360;
             }
         });
     }
