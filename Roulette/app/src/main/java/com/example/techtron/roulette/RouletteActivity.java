e92551a34960449f93ed97dc98c00b7a40c740f9
Phillip <phillipables1@gmail.com>
2018-02-14 23:14:21 -0500

    method that returns the text of the value landed on based off the fraction of degree of 360 the circle is at

--- Roulette/app/src/main/java/com/example/techtron/roulette/RouletteActivity.java
+++ Roulette/app/src/main/java/com/example/techtron/roulette/RouletteActivity.java
@@ -19,6 +19,9 @@ public class RouletteActivity extends AppCompatActivity {
 
     Random r;
     int degree = 0, degree_old = 0;
+    //his was 37 but i had an extra zero
+    //becau[se there is 38 sectors on the wheel (9.47 degrees each)
+    private static final float FACTOR = 4.7368f;
 
 
     @Override
@@ -45,12 +48,12 @@ public class RouletteActivity extends AppCompatActivity {
 
                     @Override
                     public void onAnimationStart(Animation animation) {
-
+                        textView.setText("");
                     }
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
-
+                        textView.setText(currentNumber(360 - (degree % 360)));
                     }
 
                     @Override
@@ -61,5 +64,123 @@ public class RouletteActivity extends AppCompatActivity {
                 wheelRoul.startAnimation(rotate);
             }
         });
+    }
+    private String currentNumber(int degrees) {
+        String text = "";
+        if (degrees >= (FACTOR * 1) && degrees < (FACTOR * 3)){
+            text = "27 red";
+        }
+        if (degrees >= (FACTOR * 3) && degrees < (FACTOR * 5)){
+            text = "10 black";
+        }
+        if (degrees >= (FACTOR * 5) && degrees < (FACTOR * 7)){
+            text = "35 red";
+        }
+        if (degrees >= (FACTOR * 7) && degrees < (FACTOR * 9)){
+            text = "29 black";
+        }
+        if (degrees >= (FACTOR * 9) && degrees < (FACTOR * 11)){
+            text = "12 red";
+        }
+        if (degrees >= (FACTOR * 11) && degrees < (FACTOR * 13)){
+            text = "8 black";
+        }
+        if (degrees >= (FACTOR * 13) && degrees < (FACTOR * 15)){
+            text = "19 red";
+        }
+        if (degrees >= (FACTOR * 15) && degrees < (FACTOR * 17)){
+            text = "31 black";
+        }
+        if (degrees >= (FACTOR * 17) && degrees < (FACTOR * 19)){
+            text = "18 red";
+        }
+        if (degrees >= (FACTOR * 19) && degrees < (FACTOR * 21)){
+            text = "6 black";
+        }
+        if (degrees >= (FACTOR * 21) && degrees < (FACTOR * 23)){
+            text = "21 red";
+        }
+        if (degrees >= (FACTOR * 23) && degrees < (FACTOR * 25)){
+            text = "33 black";
+        }
+        if (degrees >= (FACTOR * 25) && degrees < (FACTOR * 27)){
+            text = "16 red";
+        }
+        if (degrees >= (FACTOR * 27) && degrees < (FACTOR * 29)){
+            text = "4 black";
+        }
+        if (degrees >= (FACTOR * 29) && degrees < (FACTOR * 31)){
+            text = "23 red";
+        }
+        if (degrees >= (FACTOR * 31) && degrees < (FACTOR * 33)){
+            text = "35 black";
+        }
+        if (degrees >= (FACTOR * 33) && degrees < (FACTOR * 35)){
+            text = "14 red";
+        }
+        if (degrees >= (FACTOR * 35) && degrees < (FACTOR * 37)){
+            text = "2 black";
+        }
+        if (degrees >= (FACTOR * 37) && degrees < (FACTOR * 39)){
+            text = "zero";
+        }
+        if (degrees >= (FACTOR * 39) && degrees < (FACTOR * 41)){
+            text = "28 black";
+        }
+        if (degrees >= (FACTOR * 41) && degrees < (FACTOR * 43)){
+            text = "9 red";
+        }
+        if (degrees >= (FACTOR * 43) && degrees < (FACTOR * 45)){
+            text = "26 black";
+        }
+        if (degrees >= (FACTOR * 45) && degrees < (FACTOR * 47)){
+            text = "30 red";
+        }
+        if (degrees >= (FACTOR * 47) && degrees < (FACTOR * 49)){
+            text = "11 black";
+        }
+        if (degrees >= (FACTOR * 49) && degrees < (FACTOR * 51)){
+            text = "7 red";
+        }
+        if (degrees >= (FACTOR * 51) && degrees < (FACTOR * 53)){
+            text = "20 black";
+        }
+        if (degrees >= (FACTOR * 53) && degrees < (FACTOR * 55)){
+            text = "32 red";
+        }
+        if (degrees >= (FACTOR * 55) && degrees < (FACTOR * 57)){
+            text = "17 black";
+        }
+        if (degrees >= (FACTOR * 57) && degrees < (FACTOR * 59)){
+            text = "5 red";
+        }
+        if (degrees >= (FACTOR * 59) && degrees < (FACTOR * 61)){
+            text = "22 black";
+        }
+        if (degrees >= (FACTOR * 61) && degrees < (FACTOR * 63)){
+            text = "34 red";
+        }
+        if (degrees >= (FACTOR * 63) && degrees < (FACTOR * 65)){
+            text = "15 black";
+        }
+        if (degrees >= (FACTOR * 65) && degrees < (FACTOR * 67)){
+            text = "3 red";
+        }
+        if (degrees >= (FACTOR * 67) && degrees < (FACTOR * 69)){
+            text = "24 black";
+        }
+        if (degrees >= (FACTOR * 69) && degrees < (FACTOR * 71)){
+            text = "36 red";
+        }
+        if (degrees >= (FACTOR * 71) && degrees < (FACTOR * 73)){
+            text = "13 black";
+        }
+        if(degrees >= (FACTOR * 73) && degrees < (FACTOR * 75)){
+            text = "1 red";
+        }
+        if(degrees >= (FACTOR * 75) && degrees < 360 || degrees >= 0 && degrees < (FACTOR * 77)){
+            text = "zero";
+        }
+        return text;
     }
 }
